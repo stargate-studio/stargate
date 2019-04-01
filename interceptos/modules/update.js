20709416c2c921f711a94c778229f5e76290fdb9
Diogo Vianna <diogovaraujo@gmail.com>
2019-03-31 23:24:22 -0300

    Update update.js

--- interceptos/modules/update.js
+++ interceptos/modules/update.js
@@ -2,19 +2,26 @@ import OTA from "esp32/ota"
 import {Request} from "http"
 
 let ota = new OTA
-let request = new Request({host: "diogo.velabikes.com.br", path: "/firmware.bin"});
-request.callback = function(message) {
-    trace(message)
-	if (4 == message) {
-		let bytes = this.read(ArrayBuffer);
-		trace(bytes.byteLength, "\n");
-		ota.write(bytes);
-	}
 
-	if (5 == message) {
-		trace(`\n\nTransfer complete.\n`);
-		ota.complete();
-	}
+export function startUpdateFromUrl(url) {
+  let request = new Request({host: url, path: "/firmware.bin"})
+  request.callback = function(message) {
+    if (4 == message) {
+      let bytes = this.read(ArrayBuffer)
+      trace(bytes.byteLength, "\n")
+      ota.write(bytes)
+    }
+    if (5 == message) {
+      trace(`\n\nTransfer complete.\n`);
+      ota.complete();
+    }
+  }	
+}
+
+export function updateAvailable() {
+  //use broker to check for a update?
+  // creve message event monitor, send question and wait for answer?
+  // we need to know what version are we in.. state management? something fixed and the code?
 }
 
 export default ota
