c7163de39929230c360edf6eef37be19e5ea959e
Diogo Vianna <diogovaraujo@gmail.com>
2019-04-07 22:42:34 -0300

    Fixing indentation on eventemitter.js

--- interceptos/modules/eventemitter.js
+++ interceptos/modules/eventemitter.js
@@ -1,20 +1,20 @@
 export default function eventemitter(all) {
-	all = all || Object.create(null);
+  all = all || Object.create(null);
 
-	return {
-		on(type, handler) {
-			(all[type] || (all[type] = [])).push(handler);
-		},
+  return {
+    on(type, handler) {
+      (all[type] || (all[type] = [])).push(handler);
+    },
 
-		off(type, handler) {
-			if (all[type]) {
-				all[type].splice(all[type].indexOf(handler) >>> 0, 1);
-			}
-		},
-    
-		emit(type, evt) {
-			(all[type] || []).slice().map((handler) => { handler(evt); });
-			(all['*'] || []).slice().map((handler) => { handler(type, evt); });
-		}
-	};
+    off(type, handler) {
+      if (all[type]) {
+        all[type].splice(all[type].indexOf(handler) >>> 0, 1);
+      }
+    },
+
+    emit(type, evt) {
+      (all[type] || []).slice().map((handler) => { handler(evt); });
+      (all['*'] || []).slice().map((handler) => { handler(type, evt); });
+     }
+  };
 }
